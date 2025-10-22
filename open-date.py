16c2c99581fb5561ab8ed0b165237c11429cb6a3
liuuu20 <asdsa654556@gmail.com>
2025-10-22 23:07:18 +0800

    sec

--- open-date.py
+++ open-date.py
@@ -1,8 +1,15 @@
 #網路連線
+# import urllib.request as request
+
+#src="https://www.im.tku.edu.tw/"
+#with request.urlopen(src) as response:
+    #data=response.read().decode("utf-8")
+#print(data)
+
+#串接、擷取公開資料
 import urllib.request as request
 
-src="https://www.im.tku.edu.tw/"
+src="https://data.moa.gov.tw/Service/OpenData/TraceBeefData.aspx?IsTransData=1&UnitId=369"
 with request.urlopen(src) as response:
-    data=response.read().decode("utf-8")
+    data=json.load(response) #json.load()直接將JSON格式資料轉成Python資料結構
 print(data)
-#串接、擷取公開資料
