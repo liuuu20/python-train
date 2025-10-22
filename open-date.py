3d2e5b4e795861c9765fe977781fcb154ef5320e
liuuu20 <asdsa654556@gmail.com>
2025-10-22 23:16:30 +0800

    3

--- open-date.py
+++ open-date.py
@@ -8,8 +8,15 @@
 
 #串接、擷取公開資料
 import urllib.request as request
-
-src="https://data.moa.gov.tw/Service/OpenData/TraceBeefData.aspx?IsTransData=1&UnitId=369"
+import json
+src="https://od.moi.gov.tw/api/v1/rest/datastore/301000000A-001489-022"
 with request.urlopen(src) as response:
     data=json.load(response) #json.load()直接將JSON格式資料轉成Python資料結構
 print(data)
+
+#將公司名稱列表出來
+clist = data["result"]["results"]
+with open("company.txt","w",encoding="utf-8") as file:
+    for company in clist:
+        file.write(company["CompanyName"]+"\n")
+print("公司名稱已寫入company.txt檔案中")
